d787778000185d1628c2f9452233fbdfece1e76c
punegurukul <aabhyasukatta@gmail.com>
2023-12-31 12:07:52 +0530

    js day 1

---
+++ js/hello.js
@@ -0,0 +1,6 @@
+console.log("Hello World from js file");
+document.write('This is a new text from doc.write');
+window.alert('I am an alert');
+alert('I am an alert 2');
+document.getElementById('title').innerHTML = '<span style="color:red"> Hi </span>';
+print();
